5a877e3744b805672ed84852aeabef6040370318
jordyful4 <Jordyn.Fulton@lsu.edu>
2026-03-23 02:19:31 -0500

    here's my mini project! i'm gonna message you on groupme/email to see if we can meet at 12:30-1:30 on monday because i can't get my graphs to come out right and i need the figures for my presentation :(
    changed my project to be on how a forest fire in sierra nevada (2021) changed the area compared to 2020 bc the data was better than in arizona :p

---
+++ laketahoeminiproj.m
@@ -0,0 +1,130 @@
+%%MINI PROJECT 3/23 - WILDFIRES IN CALIFORNIA
+
+clear
+
+ filename1='before2021cali.tif';
+  filename2='after2021cali.tif';
+
+  [x1,y1,z1]=load_one_DEM(filename1);
+  [x2,y2,z2]=load_one_DEM(filename2);
+
+
+
+ CA=[0,500]; % the color axis extent for these figures
+
+  figure(2)
+  clf
+  ax(1)=subplot(121);
+  imagesc(x1,y1,z1)
+  axis xy
+  axis equal
+  colorbar
+  title('before')
+  caxis(CA)
+
+  ax(2)=subplot(122);
+  imagesc(x2,y2,z2)
+  axis xy
+  axis equal
+  colorbar
+  title('after')
+  caxis(CA)
+
+  linkaxes(ax,'xy')
+
+  % determine the extent of the overlap
+%
+  ytop=min(max(y1),max(y2));
+
+  % made up numbers because Karen is lazy
+  ybottom=max(min(y1),min(y2));
+  xleft=max(min(x1),min(x2));
+  xright=min(max(x1),max(x2));
+
+%
+% find the indices of rows and colums within
+% the overlapping region
+%
+  i1=find(y1<ytop & y1>ybottom);
+  j1=find(x1<xright & x1>xleft);
+
+  i2=find(y2<ytop & y2>ybottom);
+  j2=find(x2<xright & x2>xleft);
+
+%
+% Cut the DEM to the smaller area
+%
+  z1_smaller=z1(i1,j1);
+  z2_smaller=z2(i2,j2);
+
+  x_smaller=x1(j1);
+  y_smaller=y1(i1);
+
+%
+% Plot the new ones!
+% (plowing over the old figure)
+%
+  figure(2)
+  clf
+  ax(1)=subplot(121);
+  imagesc(x_smaller,y_smaller,z1_smaller)
+  axis xy
+  axis equal
+  colorbar
+  title('before - cut to coregistered size')
+  caxis(CA)
+
+  ax(2)=subplot(122);
+  imagesc(x_smaller,y_smaller,z2_smaller)
+  axis xy
+  axis equal
+  colorbar
+  title('after - cut to coregistered size')
+  caxis(CA)
+
+  linkaxes(ax,'xy')
+
+%
+% Difference the DEMs and plot them
+%
+  z_difference=z2_smaller-z1_smaller;
+
+  figure(3)
+  clf
+  ax(3)=subplot(121);
+  imagesc(x_smaller,y_smaller,z_difference)
+  axis xy
+  axis equal
+  colorbar
+  title('after-before difference')
+  colormap(flipud(cpolar)) % flip the colors to match fig 5c
+  caxis([-2,2])
+
+  linkaxes(ax,'xy')
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Function Definitions Start Here
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  function [x,y,a]=load_one_DEM(filename)
+    %load the file
+    [a,r]=readgeoraster(filename);
+
+    % Make the x and y vectors for the DEM grid
+    x1=r.XWorldLimits(1);
+    x2=r.XWorldLimits(2);
+    dx=r.CellExtentInWorldX;
+    x=(x1+dx/2):dx:(x2-dx/2);
+
+    y1=r.YWorldLimits(1);
+    y2=r.YWorldLimits(2);
+    dy=r.CellExtentInWorldY;
+    y=(y1+dy/2):dy:(y2-dy/2);
+
+    % find all the pseudo-NaNs and make them actual NaNs
+    iNaNs=find(a==-9999);
+    a(iNaNs)=NaN;
+
+    % flip the image upside down so that it is a proper map
+    a=flipud(a);
+  end
